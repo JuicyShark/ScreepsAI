517310a13499cb2fcbe65e0b5dd4fb2c532f4b86
MrXandbadas <kodiea@gmail.com>
2018-08-09 15:17:32 +1000

    creep claimer role file
    Claimer is waiting for a memory "target"

---
+++ src/role.claimer.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    run: function(creep) {
+      let structure = creep.room.controller;
+        if (creep.room.name != creep.memory.target) {
+            var exit = creep.room.findExitTo(creep.memory.target);
+            creep.ourPath(creep.pos.findClosestByRange(exit));
+        }
+        else {
+            if (creep.claimController(structure) == ERR_NOT_IN_RANGE) {
+                creep.ourPath(structure);
+            }
+        }
+    }
+};
