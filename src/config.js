708998f4ff9339eba3566555e470997ab859aa9b
MrXandbadas <kodiea@gmail.com>
2018-08-16 01:16:46 +1000

    Bye bye max miners

--- src/config.js
+++ src/config.js
@@ -27,16 +27,6 @@ module.exports = {
     repairer: '',
     roomPopulation: ''
   },
-  maxMiners: {
-    1: 0,
-    2: 0,
-    3: 2,
-    4: 2,
-    5: 2,
-    6: 2,
-    7: 2,
-    8: 2
-  },
   maxUpgraders: {
     1: 1,
     2: 2,
