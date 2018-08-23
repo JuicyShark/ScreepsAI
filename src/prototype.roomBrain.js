e3e8e60aba0c42d5ebb88333733e2d4685e8fb8f
Juicy-Shark <maxwellb9879@gmail.com>
2018-08-23 17:22:04 +1000

    create repair task change

--- src/prototype.roomBrain.js
+++ src/prototype.roomBrain.js
@@ -12,7 +12,7 @@ var config = require("config")
     @param {object} details   // further details of the task including target/s and any id's of things associated
    // create a task to assign in a queue
     */
-Room.prototype.createTask = function(name,  typeNeeded, priority, details) {
+Room.prototype.createTask = function(name, typeNeeded, priority, details) {
   var task = {
     name: name,
     typeNeeded: typeNeeded,
@@ -21,23 +21,23 @@ Room.prototype.createTask = function(name,  typeNeeded, priority, details) {
   }
   var duplicateTask = null;
 
-    for (var i = 0; i < Game.creeps.length; i++) {
-      if( duplicateTask == true) break;
-      if (Game.creeps[i].memory.task != null || Game.creeps[i].memory.task != undefined){
-        if(Game.creeps[i].memory.task.details.target == task.details.target){
+  for (var i = 0; i < Game.creeps.length; i++) {
+    if (duplicateTask == true) break;
+    if (Game.creeps[i].memory.task != null || Game.creeps[i].memory.task != undefined) {
+      if (Game.creeps[i].memory.task.details.target == task.details.target) {
 
-             duplicateTask = true;
-         }
-       }
+        duplicateTask = true;
+      }
     }
-    for (var i = 0; i < this.memory.taskList.length; i++) {
-      if( duplicateTask == true) break;
-      if ( this.memory.taskList[i].details.target == task.details.target){
-           duplicateTask = true;
+  }
+  for (var i = 0; i < this.memory.taskList.length; i++) {
+    if (duplicateTask == true) break;
+    if (this.memory.taskList[i].details.target == task.details.target) {
+      duplicateTask = true;
 
-         }
     }
-  if (duplicateTask != true ) {
+  }
+  if (duplicateTask != true) {
     this.memory.taskList.push(task)
   }
 }
@@ -46,15 +46,15 @@ Room.prototype.assignTasks = function() {
 
   var tooManyTask = []
 
-  for(var i = 0; i < this.memory.creepsByType.length; i++){
+  for (var i = 0; i < this.memory.creepsByType.length; i++) {
     let creepsBytype = this.memory.creepsByType[i]
-    for(var a = 0; a < creepsBytype.creeps.length; a++) {
+    for (var a = 0; a < creepsBytype.creeps.length; a++) {
       let chosenOne = Game.getObjectById(creepsBytype.creeps[a])
-      if(chosenOne instanceof Creep) {
-        if(chosenOne.memory.task == null ){
+      if (chosenOne instanceof Creep) {
+        if (chosenOne.memory.task == null) {
           let thisCreepType = chosenOne.memory.type
           var newTask = this.filtertask(thisCreepType)
-          if(newTask != null || newTask != undefined) {
+          if (newTask != null || newTask != undefined) {
             chosenOne.memory.task == newTask
           }
         }
@@ -70,169 +70,181 @@ Room.prototype.checkTask = function(type) {
 }
 
 Room.prototype.constantTasks = function() {
-  Object.keys(this.memory.sourceNodes).forEach(i => {
-  //for (var i = 0; i < this.memory.sourceNodes.length; i++) {
-    let thisSourceID = this.memory.sourceNodes[i].id;
-      if(this.memory.sourceNodes[i].container == null){
-        let details = { target: thisSourceID }
-        if(thisSourceID != null){
-        this.createTask("HARVEST", "ALL_ROUND", 1, details)}
-    } else if (this.memory.sourceNodes[i].container != null) {
+    Object.keys(this.memory.sourceNodes).forEach(i => {
+      //for (var i = 0; i < this.memory.sourceNodes.length; i++) {
+      let thisSourceID = this.memory.sourceNodes[i].id;
+      if (this.memory.sourceNodes[i].container == null) {
+        let details = {
+          target: thisSourceID
+        }
+        if (thisSourceID != null) {
+          this.createTask("HARVEST", "ALL_ROUND", 1, details)
+        }
+      } else if (this.memory.sourceNodes[i].container != null) {
         let thisSourceContainer = this.memory.sourceNodes[i].container;
-        let details = { target: thisSourceContainer, sourceId: thisSourceID}
-        this.createTask("CONTAINER_MINE", "CONTAINER_MINER", 1, details )
-    }
-  })
+        let details = {
+          target: thisSourceContainer,
+          sourceId: thisSourceID
+        }
+        this.createTask("CONTAINER_MINE", "CONTAINER_MINER", 1, details)
+      }
+    })
 
-  if(this.memory.structureIDs.controller.id != null) {
-    details = { target: this.memory.structureIDs.controller.id }
-    this.createTask("UPGRADE", "UPGRADER", 1, details)
-  }
-  if(this.memory.structureIDs.Spawns.length >= 1){
-      details = {target: this.memory.structureIDs.Spawns[0] }
-      this.createTask("REPAIR", "ALL_ROUND", 2, details)
+    if (this.memory.structureIDs.controller.id != null) {
+      details = {
+        target: this.memory.structureIDs.controller.id
+      }
+      this.createTask("UPGRADE", "UPGRADER", 1, details)
     }
-    if(this.memory.structureIDs.Towers.length >= 1){
-        details = {target: this.memory.structureIDs.Towers[0] }
-        this.createTask("REPAIR", "ALL_ROUND", 1, details)
+
+    for (var s of this.find(FIND_STRUCTURES, {
+        filter: function(i) {
+          return i.needsRepair(true);
+        }
+      })) {
+        console.log(s.pos)
+        var details = {
+          target: s.pos
+        }
+      if (!s instanceof StructureWall && !s instanceof StructureRampart) {
+        let task = this.createTask('REPAIR', "ALL_ROUND", 2, details);
+      } else {
+
+        let task = this.createTask('REPAIR', "ALL_ROUND", 4, details);
+      }
+
+    }
+}
+
+    Room.prototype.isMine = function() {
+      return this.controller && this.controller.my;
+    }
+
+    Room.prototype.alertLevel = function() {
+      let hostiles = this.find(FIND_HOSTILE_CREEPS, {
+        filter: {
+          owner: {
+            username: !config.allies.username
+          }
+        }
+      });
+      if (hostiles.size() >= 1) {
+        this.safeGuardUp()
+        return 2;
       }
     }
 
+    Room.prototype.processAsGuest = function() {
 
-Room.prototype.isMine = function() {
-  return this.controller && this.controller.my;
-}
+    }
 
-Room.prototype.alertLevel = function() {
-  let hostiles = this.find(FIND_HOSTILE_CREEPS, {
-    filter: {
-      owner: {
-        username: !config.allies.username
+
+
+    Room.prototype.needSourceScouts = function() {
+      let scoutRoomFlags = [];
+      for (var i = 0; i < Memory.flags.length; i++) {
+        if (Memory.flags[i].needScout) {
+          scoutRoomFlags.push(Memory.flags[i].id)
+          scoutRoomFlags.push(Memory.flags[i])
+        }
+      }
+      if (scoutRoomFlags.length != 0) {
+        return scoutRoomFlags;
+      } else {
+        return false;
       }
     }
-  });
-  if (hostiles.size() >= 1) {
-    this.safeGuardUp()
-    return 2;
-  }
-}
 
-Room.prototype.processAsGuest = function() {
-
-}
-
-
-
-Room.prototype.needSourceScouts = function() {
-  let scoutRoomFlags = [];
-  for (var i = 0; i < Memory.flags.length; i++) {
-    if (Memory.flags[i].needScout) {
-      scoutRoomFlags.push(Memory.flags[i].id)
-      scoutRoomFlags.push(Memory.flags[i])
+    Room.prototype.needBasicWorker = function() {
+      if (this.memory.creepsByType.allRound.creeps.length == 0) {
+        return true
+      } else if (this.memory.creepsByType.allRound.creeps.length >= 3 && this.needContainerMiner() == true) {
+        return false
+      } else if (this.memory.creepsByType.allRound.creeps.length > 4) {
+        return false
+      } else if (this.memory.creepsByType.allRound.creeps.length <= 3 && this.needContainerMiner() == false) {
+        return true
+      } else if (this.memory.creepsByType.allRound.creeps.length <= 6 && this.level() >= 5) {
+        return true
+      }
     }
-  }
-  if (scoutRoomFlags.length != 0) {
-    return scoutRoomFlags;
-  } else {
-    return false;
-  }
-}
+    // Testing to only want harvesters if we dont have miners and lorrys around
+    Room.prototype.needLorry = function() {
+      if (this.memory.creepsByType.containerMiner.creeps.length == 0) {
+        return false
+      } else if (this.memory.creepsByType.containerMiner.creeps.length != 0) {
+        if (this.memory.creepsByType.lorry.creeps.length <= 2) {
+          return true
+        }
+      }
 
-Room.prototype.needBasicWorker = function() {
-  if(this.memory.creepsByType.allRound.creeps.length == 0) {
-    return true
-  }
-  else if(this.memory.creepsByType.allRound.creeps.length >= 3 && this.needContainerMiner() == true){
-    return false
-  }
-  else if (this.memory.creepsByType.allRound.creeps.length > 4) {
-    return false
-  }
-  else if(this.memory.creepsByType.allRound.creeps.length <= 3 && this.needContainerMiner() == false){
-    return true
-  }
-  else if(this.memory.creepsByType.allRound.creeps.length <= 6 && this.level() >= 5) {
-    return true
-  }
-}
-// Testing to only want harvesters if we dont have miners and lorrys around
-Room.prototype.needLorry = function() {
-  if(this.memory.creepsByType.containerMiner.creeps.length == 0) {
-    return false
-  } else if (this.memory.creepsByType.containerMiner.creeps.length != 0) {
-    if (this.memory.creepsByType.lorry.creeps.length <= 2 ) {
-      return true
     }
-  }
 
-}
+    Room.prototype.needUpgrader = function() {
+      let UpgraderMemCount = this.memory.creepsByType.upgrader.creeps.length;
+      if (this.level() >= 3) {
+        if (UpgraderMemCount == 0 || UpgraderMemCount <= 2) {
+          return true
+        }
+      } else if (this.level() <= 3) {
+        if (UpgraderMemCount == 0 && UpgraderMemCount <= 4) {
+          return true
+        }
+      }
+    }
+    Room.prototype.needContainerMiner = function() {
+      let output = []
+      for (var i = 0; i < this.memory.sourceNodes.length; i++) {
+        let thisSourceID = this.memory.sourceNodes[i].id;
+        if (this.memory.sourceNodes[i].container == null) {
 
-Room.prototype.needUpgrader = function() {
-  let UpgraderMemCount = this.memory.creepsByType.upgrader.creeps.length;
-  if(this.level() >=3) {
-    if (UpgraderMemCount == 0 || UpgraderMemCount <= 2){
-      return true
+        } else if (this.memory.sourceNodes[i].container != null) {
+          output.push(thisSourceID)
+        }
+      }
+      if (this.memory.creepsByType.containerMiner.creeps.length >= output.length) {
+        return false
+      } else if (this.memory.creepsByType.containerMiner.creeps.length <= output.length) {
+        return true
+      }
     }
-  }
-  else if (this.level() <=3) {
-    if(UpgraderMemCount == 0 && UpgraderMemCount <= 4){
-      return true
-    }
-  }
-}
-Room.prototype.needContainerMiner = function() {
-  let output = []
-  for (var i = 0; i < this.memory.sourceNodes.length; i++) {
-    let thisSourceID = this.memory.sourceNodes[i].id;
-      if(this.memory.sourceNodes[i].container == null){
 
-    } else if (this.memory.sourceNodes[i].container != null) {
-        output.push(thisSourceID)
+    Room.prototype.needDefender = function() {
+      /*  if () {
+          return true
+        }
+        else {
+          return false
+        }*/
     }
-  }
-  if(this.memory.creepsByType.containerMiner.creeps.length >= output.length){
-    return false
-  } else if (this.memory.creepsByType.containerMiner.creeps.length <= output.length) {
-    return true
-  }
-}
 
-Room.prototype.needDefender = function() {
-  /*  if () {
-      return true
-    }
-    else {
+    Room.prototype.needRangedAttacker = function() {
       return false
-    }*/
-}
+      //attacking logic
+    }
 
-Room.prototype.needRangedAttacker = function() {
-  return false
-  //attacking logic
-}
+    Room.prototype.needBrawler = function() {
 
-Room.prototype.needBrawler = function() {
+      return false
+      //attacking logic
+    }
 
-  return false
-  //attacking logic
-}
+    Room.prototype.needMedic = function() {
+      return false
+      //attacking logic
+    }
 
-Room.prototype.needMedic = function() {
-  return false
-  //attacking logic
-}
-
-Room.prototype.needClaimer = function() {
-  if (this.energyCapacityAvailable >= 700) {
-    for (let i in Memory.flags) {
-      let = thisFlag = Memory.flags[i]
-      if (thisFlag.name == "ClaimRoom" && thisFlag.hasClaimer == false) {
-        var spawn = this.find(FIND_MY_SPAWNS)
-        spawn[0].spawnClaimer(roomName, thisFlag)
+    Room.prototype.needClaimer = function() {
+      if (this.energyCapacityAvailable >= 700) {
+        for (let i in Memory.flags) {
+          let = thisFlag = Memory.flags[i]
+          if (thisFlag.name == "ClaimRoom" && thisFlag.hasClaimer == false) {
+            var spawn = this.find(FIND_MY_SPAWNS)
+            spawn[0].spawnClaimer(roomName, thisFlag)
+          }
+        }
+        return true;
+      } else {
+        return false;
       }
     }
-    return true;
-  } else {
-    return false;
-  }
-}
