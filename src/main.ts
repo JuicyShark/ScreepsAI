6218304f8462995f67dfdd38a0c2dbe877603b51
Juicy-Shark <maxwellb9879@gmail.com>
2018-08-26 14:06:41 +1000

    screeps profiler

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ErrorMapper } from "utils/ErrorMapper";
 import { Traveler } from "utils/Traveler";
-
+const profiler = require('screeps-profiler');
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
+profiler.enable();
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
+    profiler.wrap(function() {
+    console.log(`Current game tick is ${Game.time}`);
 
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
@@ -12,4 +14,5 @@ export const loop = ErrorMapper.wrapLoop(() => {
       delete Memory.creeps[name];
     }
   }
+    });
 });
