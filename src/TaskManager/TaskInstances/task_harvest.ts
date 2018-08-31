c532d7981fa86e73d661213b3039afc0c86b2288
Juicy-Shark <maxwellb9879@gmail.com>
2018-09-01 01:48:10 +1000

    missed some files

--- src/TaskManager/TaskInstances/task_harvest.ts
+++ src/TaskManager/TaskInstances/task_harvest.ts
@@ -1,10 +1,8 @@
 import {Task} from '../Task';
+import {isSource} from '../../declarations/typeGuards'
 
 export type harvestTargetType = Source | Mineral;
-
-function isSource(obj: Source | Mineral): obj is Source {
-	return (<Source>obj).energy != undefined;
-}
+export const harvestTaskName = 'harvest';
 
 export class TaskHarvest extends Task {
 
@@ -12,7 +10,7 @@ export class TaskHarvest extends Task {
 	target!: harvestTargetType;
 
 	constructor(target: harvestTargetType, options = {} as TaskOptions) {
-		super(TaskHarvest.taskName, target, options);
+		super(harvestTaskName, target, options);
 	}
 
 	isValidTask() {
@@ -20,11 +18,6 @@ export class TaskHarvest extends Task {
 	}
 
 	isValidTarget() {
-		// if (this.target && (this.target instanceof Source ? this.target.energy > 0 : this.target.mineralAmount > 0)) {
-		// 	// Valid only if there's enough space for harvester to work - prevents doing tons of useless pathfinding
-		// 	return this.target.pos.availableNeighbors().length > 0 || this.creep.pos.isNearTo(this.target.pos);
-		// }
-		// return false;
 		if (isSource(this.target)) {
 			return this.target.energy > 0;
 		} else {
