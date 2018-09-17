f4d3f1dd2ecc7bf304c8766671ea6c9f00150246
MrXandbadas <kodiea@gmail.com>
2018-09-17 11:16:20 +1000

    fixxy

--- src/ShowMaster/roomMaster.ts
+++ src/ShowMaster/roomMaster.ts
@@ -231,50 +231,50 @@ export class RoomBrain {
       }
 
     }
-  }
+
 
     var harvesters: Creep[] | undefined = thisColony().creepsByType.Harvester
-var upgraders: Creep[] | undefined = thisColony().creepsByType.Upgrader
-var builders: Creep[] | undefined = thisColony().creepsByType.Builder
-var containerMiner: Creep[] | undefined = thisColony().creepsByType.Miner
+    var upgraders: Creep[] | undefined = thisColony().creepsByType.Upgrader
+    var builders: Creep[] | undefined = thisColony().creepsByType.Builder
+    var containerMiner: Creep[] | undefined = thisColony().creepsByType.Miner
 
-if (room.creeps.length == 0 || harvesters == undefined) {
-  madeToOrder("Harvester", room, null)
-}
+    if (room.creeps.length == 0 || harvesters == undefined) {
+      madeToOrder("Harvester", room, null)
+    }
 
-if (harvesters != undefined && harvesters.length < room.sources.length) {
-  madeToOrder("Harvester", room, null)
-}
-if (upgraders == undefined || upgraders.length < room.controller.level) {
-  madeToOrder("Upgrader", room, null)
-}
-if (room.containers.length != 0) {
-  if (room.containers[0].targetedBy.length >= 1) {
+    if (harvesters != undefined && harvesters.length < room.sources.length) {
+      madeToOrder("Harvester", room, null)
+    }
+    if (upgraders == undefined || upgraders.length < room.controller.level) {
+      madeToOrder("Upgrader", room, null)
+    }
+    if (room.containers.length != 0) {
+      if (room.containers[0].targetedBy.length >= 1) {
 
-  }
-  room.sources.forEach(function (source: Source, index: number, array: Source[]) {
-    if (source.hasContainer() == true) {
-      let Container = source.pos.findClosestByLimitedRange(room.containers, 2)
-      let Miners: Creep | undefined = source.pos.findClosestByLimitedRange(room.creepsByType.Miner, 1)
-
-      if (Miners == undefined && Container.targetedBy.length <= 1) {
-        let ContainerID = source.pos.findClosestByLimitedRange(room.containers, 2).id
-        let options = {
-          memory: {
-            myContainer: ContainerID
-          }
-        }
-        madeToOrder("Miner", room, options)
       }
+      room.sources.forEach(function (source: Source, index: number, array: Source[]) {
+        if (source.hasContainer() == true) {
+          let Container = source.pos.findClosestByLimitedRange(room.containers, 2)
+          let Miners: Creep | undefined = source.pos.findClosestByLimitedRange(room.creepsByType.Miner, 1)
+
+          if (Miners == undefined && Container.targetedBy.length <= 1) {
+            let ContainerID = source.pos.findClosestByLimitedRange(room.containers, 2).id
+            let options = {
+              memory: {
+                myContainer: ContainerID
+              }
+            }
+            madeToOrder("Miner", room, options)
+          }
+
+        }
+      })
 
     }
-  })
 
-}
-
-if (builders == undefined || builders.length < 2) {
-  madeToOrder("Builder", room, null)
-}
+    if (builders == undefined || builders.length < 2) {
+      madeToOrder("Builder", room, null)
+    }
 
 
     //kodie flesh this out..
@@ -282,76 +282,41 @@ if (builders == undefined || builders.length < 2) {
 
 
   static spawnTaskAssigner(room: Room): void {
-  if(room.energyAvailable == room.energyCapacityAvailable) {
-  this.spawnerGo(room)
-}
+    if (room.energyAvailable == room.energyCapacityAvailable) {
+      this.spawnerGo(room)
+    }
   }
 
-
-  static run(): void {
-  for(let name in Game.rooms) {
-  const room = Game.rooms[name];
-  let roomType = room.roomType
-
-  // Record location of permanent objects in room and recompute score as needed
-  if (!room.memory.expiration || Game.time > room.memory.expiration) {
-    this.recordPermanentObjects(room);
-
-    // Refresh cache
-    let recacheTime = room.owner ? OWNED_RECACHE_TIME : RECACHE_TIME;
-    room.memory.expiration = getCacheExpiration(recacheTime, 250);
-  }
-  if (!room.memLog) {
-    room.memLog
-  }
-  //what else can we put in here?
-
-}
-if (harvesters != undefined && harvesters.length < room.sources.length) {
-  madeToOrder("Harvester", room, null)
-}
-if (upgraders == undefined || upgraders.length < 1) {
-  madeToOrder("Upgrader", room, null)
-}
-if (builders == undefined || builders.length < 4) {
-  madeToOrder("Builder", room, null)
-}
-    //kodie flesh this out..
-  }
-
-<<<<<<< HEAD
-  }
-
-=======
->>>>>>> 10b903d77be50924ad723fa022e8e82ec07f1c1d
-  static runType(room: Room, roomType: string) {
-  if (roomType == "ColonyHub") {
-    this.spawnTaskAssigner(room)
-  }
-}
-<<<<<<< HEAD
-=======
 
   static run(): void {
     for (let name in Game.rooms) {
       const room = Game.rooms[name];
       let roomType = room.roomType
+
       // Record location of permanent objects in room and recompute score as needed
       if (!room.memory.expiration || Game.time > room.memory.expiration) {
         this.recordPermanentObjects(room);
+
         // Refresh cache
         let recacheTime = room.owner ? OWNED_RECACHE_TIME : RECACHE_TIME;
         room.memory.expiration = getCacheExpiration(recacheTime, 250);
       }
-      //checking for type of room
-      for (let i in roomTypes) {
-        if (roomType == roomTypes[i]) {
-          this.runType(room, roomTypes[i])
-        }
+      if (!room.memLog) {
+        room.memLog
       }
       //what else can we put in here?
+
+    }
+
+  }
+
+
+
+  static runType(room: Room, roomType: string) {
+    if (roomType == "ColonyHub") {
+      this.spawnTaskAssigner(room)
     }
   }
->>>>>>> 10b903d77be50924ad723fa022e8e82ec07f1c1d
 }
+
 
