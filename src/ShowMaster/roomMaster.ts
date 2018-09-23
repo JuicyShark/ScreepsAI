0a484287bc188104259ab34d0a1cd684852bce6d
MrXandbadas <kodiea@gmail.com>
2018-09-23 17:33:17 +1000

    knew somthing was missing

--- src/ShowMaster/roomMaster.ts
+++ src/ShowMaster/roomMaster.ts
@@ -23,7 +23,7 @@ export class RoomBrain {
       savedSources.push({
         c: source ? source.pos.coordName : undefined,
         contnr: container ? container.pos.coordName : undefined,
-        miner: container ? container.hasMiner() : null
+        miner: source ? source.hasMiner() : false
 
       });
     }
