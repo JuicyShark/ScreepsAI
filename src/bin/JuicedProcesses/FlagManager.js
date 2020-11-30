d911cd119c28197d7d9c4583637a303f314dc6b7
MrXandbadas <kodiea@gmail.com>
2020-11-30 14:42:07 +1100

    Flagmanager Groundwork
    Here im just setting out basic logic

--- src/bin/JuicedProcesses/FlagManager.js
+++ src/bin/JuicedProcesses/FlagManager.js
@@ -30,8 +30,18 @@ export default class FlagManager extends BaseProcess {
     return Game.rooms[this.roomName]
   }
 
+  /**
+   *  Runs the Flag Logic. Located in FlagManager.js
+   */
   run() {
-     /** if (Game.flags.claim) {
+
+    //using for f
+    for (flag in Game.flags) {
+
+      this.checkFlag(flag)
+
+    }
+    /** if (Game.flags.claim) {
       let { pos: { x, y, roomName } } = Game.flags.claim
       let room = Game.rooms[roomName]
       if (room && room.controller.my) {
@@ -53,5 +63,68 @@ export default class FlagManager extends BaseProcess {
     } */
   }
 
-  
+  /**
+   * Checks against the predefined logic cases.
+   * @param flag - Takes the Flag object
+   */
+  checkFlag(flag) {
+    let ab = flag.name
+    let name = ab.toLowerCase()
+
+    switch (name) {
+      case "mining":
+        this.ldm(flag)
+        break;
+      case "attack":
+        this.attack(flag)
+        break;
+      case "defend":
+        this.defend(flag)
+        break;
+        //Default is to get and keep vision in the room if there isnt any name on the flag
+      default:
+        this.keepVision(flag)
+        break;
+    }
+
+    //anymore logic to be done after
+
+  }
+
+  /**
+   *  LDM LONG DISTANCE MINING
+   *  This one is gonna be the bees knees
+   *  @param flag Flag Object
+   */
+  ldm(flag) {
+
+  }
+
+  /**
+   * This will do some attack logic on an enemy room or if you just want a bunch of trigger happy attack creeps in a room... for "Defence"
+   *  @param flag Flag Object
+   */
+  attack(flag){
+
+  }
+
+  /**
+   * This will prepair a load of creeps to be sent off as a defending party..
+   *  @param flag Flag Object
+   */
+  defend(flag){
+
+  }
+
+  /**
+   * This will hold the logic for making sure we keep vision in a room where a flag is
+   * @param flag Flag Object
+   */
+  keepVision(flag){
+
+    //flag.room == undefined then we dont have vision. RoomObject unseeable..
+
+  }
+
+
 }
