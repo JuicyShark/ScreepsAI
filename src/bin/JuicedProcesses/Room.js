6ba44c6a74bf9d5f2ab336d9ce3dc1a7d00d4cc1
MrXandbadas <kodiea@gmail.com>
2020-11-30 15:03:30 +1100

    Sweeping things into functions
    just a lil ol clean up here

--- src/bin/JuicedProcesses/Room.js
+++ src/bin/JuicedProcesses/Room.js
@@ -42,17 +42,40 @@ export default class Room extends BaseProcess {
       this.log.warn(`Invalid Room, terminating. (${this.roomName},${JSON.stringify(this.memory)})`)
       this.kernel.killProcess(this.context.id)
     }
+
     this.sleep.sleep(5)
+
+
     const children = [
       ['JuicedProcesses/harvestManager', { room: this.roomName }],
       ['JuicedProcesses/upgradeManager', { room: this.roomName }],
       ['JuicedProcesses/towerDefense', { room: this.roomName }],
         ]
+
+    
     each(children, ([child, context = {}]) => {
       this.ensureChild(child, child, context)
     })
+    
+
+    this.feederOrganiser()
+    this.builderOrganiser()
+
+    const hostiles = this.room.find(C.FIND_HOSTILE_CREEPS).filter(IFF.notFriend)
+    this.checkHostiles(hostiles)
+
+    this.cleanChildren()
+
+
+  }
+
+  /**
+   * Spawns a feeder if needed
+   */
+  feederOrganiser() {
     let [container] = this.room.lookNear(C.LOOK_STRUCTURES, this.room.find(C.FIND_STRUCTURES).filter((s) => s.structureType === C.STRUCTURE_CONTAINER))
     let storage = this.room.find(C.FIND_STRUCTURES).filter(s => s.structureType === C.STRUCTURE_STORAGE && s.hits < (s.hitsMax / 1.5))
+
     if(container || storage){
     var feeders = 1;
     for (let i = 0; i < feeders; i++) {
@@ -71,6 +94,13 @@ export default class Room extends BaseProcess {
       })
     }
   }
+  }
+
+  /**
+   * Honestly just sick of seeing everyinthing in one damn run function
+   * Spawns builder if there are construction sites
+   */
+  builderOrganiser(){
     if (this.room.find(C.FIND_MY_CONSTRUCTION_SITES).length) {
       const cid = this.ensureCreep('builder_1', {
         rooms: [this.roomName],
@@ -84,7 +114,15 @@ export default class Room extends BaseProcess {
           base: ['builder', this.roomName]
        })
     }
-    const hostiles = this.room.find(C.FIND_HOSTILE_CREEPS).filter(IFF.notFriend)
+
+  }
+
+
+  /**
+   * checks for hostiles and ensures the spawn of a creep if needed?
+   * @param {*} hostiles  
+   */
+  checkHostiles(hostiles){
     if (hostiles.length) {
       if (true || hostiles[0].owner.username === 'Invader') {
         const cid = this.ensureCreep('protector_1', {
@@ -101,26 +139,8 @@ export default class Room extends BaseProcess {
         })
       }
     }
-
-    const attackFlags = filter(Game.flags, flag => flag.color === COLOR_RED)
-    if(attackFlags.length){   
-        const cid = this.ensureCreep('protector_2', {
-          rooms: [this.roomName],
-          body: [
-            expand([2, C.ATTACK, 2, C.MOVE]),
-            expand([1, C.ATTACK, 1, C.MOVE])
-          ],
-          priority: 0
-        })
-        this.ensureChild(`protector_${cid}`, 'JuicedProcesses/stackStateCreep', {
-          spawnTicket: cid,
-          base: ['protector', attackFlags[0].pos.roomName]
-        })
-        attackFlags[0].remove();
-    }
-
-    this.cleanChildren()
   }
+
   toString () {
     return `${this.roomName} ${this.room.level}/${this.room.controller.level}`
   }
