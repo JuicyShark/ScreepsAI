fa365117651752a1baac6b93f74711407a93afc6
MrXandbadas <kodiea@gmail.com>
2020-12-08 02:59:17 +1100

    Changing naming that got missed

--- src/bin/JuicedProcesses/index.js
+++ src/bin/JuicedProcesses/index.js
@@ -3,7 +3,7 @@ import Colony from './Colony'
 import StackStateCreep from './StackStateCreep'
 import HarvestManager from './HarvestManager'
 import UpgradeManager from './UpgradeManager'
-import TowerDefense from './RoomDefense'
+import RoomDefense from './RoomDefense'
 import Intel from './Intel'
 import SeasonalBrain from './SeasonalBrain'
 import FlagManager from './FlagManager'
@@ -18,7 +18,7 @@ export const bundle = {
     processRegistry.register('JuicedProcesses/stackStateCreep', StackStateCreep)
     processRegistry.register('JuicedProcesses/harvestManager', HarvestManager)
     processRegistry.register('JuicedProcesses/upgradeManager', UpgradeManager)
-    processRegistry.register('JuicedProcesses/roomDefense', TowerDefense)
+    processRegistry.register('JuicedProcesses/roomDefense', RoomDefense)
     processRegistry.register('JuicedProcesses/intel', Intel)
     processRegistry.register('JuicedProcesses/SeasonalBrain', SeasonalBrain)
     processRegistry.register('JuicedProcesses/flagManager', FlagManager)
